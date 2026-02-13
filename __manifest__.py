196f7e8dabeb3c772a227b6cb00e8ed1036b9d7e
geoffrey-prelium <geoffrey.lecluse45@gmail.com>
2026-02-13 16:06:03 +0100

    website

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Website Popin & Banner Builder',
-    'version': '1.3',
+    'version': '1.4',
     'author': 'Prelium',
     'category': 'Website/Marketing',
     'summary': 'Create and manage marketing popins, modals, and banners on your website.',
