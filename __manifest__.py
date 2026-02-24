481a573d5ea017a324216b4ff34992394b7fbabf
geoffrey-prelium <geoffrey.lecluse45@gmail.com>
2026-02-24 16:25:14 +0100

    fix: convert snippet to structure section for global dropzone capability

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Website Popin & Banner Builder',
-    'version': '1.7',
+    'version': '1.8',
     'author': 'Prelium',
     'category': 'Website/Marketing',
     'summary': 'Create and manage marketing popins, modals, and banners on your website.',
